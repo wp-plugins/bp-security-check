0b0fe4494ea95ca5a35d7b983ccefe19ef15cc34
bungeshea <bungeshea@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-22 04:24:08 +0000

    Tagging 1.1.0
    git-svn-id: https://plugins.svn.wordpress.org/bp-security-check/trunk@862859 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- bp-security-check.php
+++ bp-security-check.php
@@ -6,34 +6,61 @@
  * Description: Help combat spam registrations by forcing the user to answer a simple math sum while registering for your BuddyPress-powered site
  * Author:      Shea Bunge
  * Author URI:  http://bungeshea.com
- * Version:     1.0.1
+ * Version:     1.1.0
  * License:     MIT
  * License URI: http://opensource.org/licenses/MIT
  */
 
+/**
+ * Adds a maths sum to the BuddyPress registration page that the user
+ * must answer correctly before registering
+ * @version 1.1.0
+ * @license http://opensource.org/licenses/MIT MIT
+ * @author Shea Bunge (http://bungeshea.com)
+ */
 class BuddyPress_Security_Check {
 
-	static $prefix = '';
+	/**
+	 * The prefix prepended to for form fields
+	 * @var string
+	 */
+	private $prefix = '';
 
-	static function init() {
-		add_action( 'bp_signup_validate', array( __CLASS__, 'check_validation' ) );
-		add_action( 'bp_after_signup_profile_fields', array( __CLASS__, 'show_input_field' ) );
+	/**
+	 * Initialize variables and register hooks
+	 * @param string $prefix
+	 */
+	public function __construct( $prefix ) {
+		add_action( 'bp_signup_validate', array( $this, 'check_validation' ) );
+		add_action( 'bp_after_signup_profile_fields', array( $this, 'show_input_field' ) );
 
-		self::$prefix = apply_filters( 'bp_security_check_prefix', 'security_question' );
+		$this->prefix = $prefix;
 	}
 
 
-	static function get_field_value( $field_name ) {
-		$field_value = $_POST[ self::$prefix . '_' . $field_name ];
+	/**
+	 * Retrieve the value for a form field
+	 * @param  string $field_name The name of the field to retrieve the value for
+	 * @return string             The field's value
+	 */
+	private function get_field_value( $field_name ) {
+		$field_value = $_POST[ $this->prefix . $field_name ];
 		return apply_filters( 'bp_security_check_get_field_value', $field_value, $field_name );
 	}
 
-	static function do_sum( $a, $b, $op ) {
-		switch( $op ) {
-			case 1: // addition
+	/**
+	 * Retrieve the answer to a maths problem
+	 * @param  integer $a  The first number
+	 * @param  integer $b  The second number
+	 * @param  integer $op The operator code
+	 * @return integer     The problem's answer
+	 */
+	private function do_sum( $a, $b, $op ) {
+		switch ( $op ) {
 			default:
+			case 1: // addition
 				return $a + $b;
-			case 2: // subraction
+			case 2: // subtraction
 				return $a - $b;
 			case 3: // multiplication
 				return $a * $b;
@@ -42,44 +69,52 @@ class BuddyPress_Security_Check {
 		}
 	}
 
-	static function format_operation( $op ) {
+	/**
+	 * Retrieve the HTML entity for an operation
+	 * @param  integer $op An operator code
+	 * @return string      An HTML entity
+	 */
+	private function format_operation( $op ) {
 
-		switch ( $op ) {
-			case 1: // addition
-			default:
-				return '&#43;';
-			case 2: // subraction
-				return '&#8722;';
-			case 3: // multiplication
-				return '&#215;';
-			case 4: // division
-				return '&#247;';
-		}
+		$ops = array(
+			1 => '&#43;',   // addition
+			2 => '&#8722;', // subtraction
+			3 => '&#215;',  // multiplication
+			4 => '&#247;',  // division
+		);
+
+		return ( isset( $ops[ $op ] ) ? $ops[ $op ] : $ops[1] );
 	}
 
-	static function check_validation(){
+	/**
+	 * Check if the user's input was correct
+	 */
+	public function check_validation(){
 		global $bp;
 
-		$number_a  = intval( self::get_field_value( 'number_a' ) );
-		$number_b  = intval( self::get_field_value( 'number_b' ) );
-		$answer    = intval( self::get_field_value( 'answer' ) );
-		$operation = self::get_field_value( 'operation' );
+		$number_a  = intval( $this->get_field_value( 'number_a' ) );
+		$number_b  = intval( $this->get_field_value( 'number_b' ) );
+		$answer    = intval( $this->get_field_value( 'answer' ) );
+		$operation = $this->get_field_value( 'operation' );
 
-		if ( self::do_sum( $number_a, $number_b, $operation ) !== $answer ) {
+		if ( $this->do_sum( $number_a, $number_b, $operation ) !== $answer ) {
 			/* The submitted answer was incorrect */
-			$bp->signup->errors['security_check'] = __('Sorry, please answer the question again','buddypress');
+			$bp->signup->errors['security_check'] = __( 'Sorry, please answer the question again', 'bp-security-check' );
 		}
 		elseif ( empty( $answer ) ) {
 			/* The answer field wasn't filled in */
-			$bp->signup->errors['security_check'] = __('This is a required field','buddypress');
+			$bp->signup->errors['security_check'] = __( 'This is a required field', 'bp-security-check' );
 		}
 	}
 
-	static function show_input_field() {
+	/**
+	 * Render the input fields
+	 */
+	public function show_input_field() {
 
 		/* Get a random number between 0 and 10 (inclusive) */
-		$a = rand( 0, 10 );
-		$b = rand( 0, 10 );
+		$a = mt_rand( 0, 10 );
+		$b = mt_rand( 0, 10 );
 
 		/* Make sure that $a is greater then $b; if not, switch them */
 		if ( $b > $a ) {
@@ -87,25 +122,34 @@ class BuddyPress_Security_Check {
 			$a = $b;      // assign $a (lower number) to $b (higher number)
 			$b = $_a;     // assign $b to the original $a
 			unset( $_a ); // destroy the backup variable
+		} elseif ( $a == $b ) {
+			$a++;  // Increment $a so that we never get 0 and hit validation errors being required
 		}
-
 		/* Get a random operation */
-		$op = rand( 1, 2 );
+		$op = mt_rand( 1, 2 );
 
 		?>
 		<div style="float: left; clear: left; width: 48%; margin: 12px 0;" class="security-question-section">
-			<h4><?php esc_html_e('Security Question', 'buddypress'); ?></h4>
+			<h4><?php esc_html_e( 'Security Question', 'bp-security-check' ); ?></h4>
 			<?php do_action( 'bp_security_check_errors' ); ?>
-			<label for="<?php echo self::$prefix; ?>_answer" style="display: inline;">
-				<?php printf( '%1$d %3$s %2$d &#61;', $a, $b, self::format_operation( $op ) ); ?>
+			<label for="<?php echo $this->prefix; ?>answer" style="display: inline;">
+				<?php printf( '%1$d %3$s %2$d &#61;', $a, $b, $this->format_operation( $op ) ); ?>
 			</label>
-			<input type="hidden" name="<?php echo self::$prefix; ?>_number_a" value="<?php echo $a; ?>" />
-			<input type="hidden" name="<?php echo self::$prefix; ?>_number_b" value="<?php echo $b; ?>" />
-			<input type="hidden" name="<?php echo self::$prefix; ?>_operation" value="<?php echo $op; ?>" />
-			<input type="number" name="<?php echo self::$prefix; ?>_answer" min="0" max="20" required="required" />
+			<input type="hidden" name="<?php echo $this->prefix; ?>number_a" value="<?php echo $a; ?>" />
+			<input type="hidden" name="<?php echo $this->prefix; ?>number_b" value="<?php echo $b; ?>" />
+			<input type="hidden" name="<?php echo $this->prefix; ?>operation" value="<?php echo $op; ?>" />
+			<input type="number" name="<?php echo $this->prefix; ?>answer" min="0" max="20" required="required" />
 		</div>
 		<?php
 	}
 }
 
-add_action( 'bp_init', array( 'BuddyPress_Security_Check', 'init' ) );
+/**
+ * Initialize the plugin class
+ */
+function bp_security_check_init() {
+	$prefix = apply_filters( 'bp_security_check_prefix', 'security_question_' );
+	$GLOBALS['bp_security_check'] = new BuddyPress_Security_Check( $prefix );
+}
+
+add_action( 'bp_init', 'buddypress_security_check_init' );
