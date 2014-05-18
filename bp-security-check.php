d1a5c323f433c90876b7e020711b29c09ccc53db
bungeshea <bungeshea@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-18 05:12:47 +0000

    Version 1.3.0
    git-svn-id: https://plugins.svn.wordpress.org/bp-security-check/trunk@916381 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- bp-security-check.php
+++ bp-security-check.php
@@ -6,115 +6,84 @@
  * Description: Help combat spam registrations by forcing the user to answer a simple math sum while registering for your BuddyPress-powered site
  * Author:      Shea Bunge
  * Author URI:  http://bungeshea.com
- * Version:     1.1.0.1
+ * Version:     1.2.0
  * License:     MIT
  * License URI: http://opensource.org/licenses/MIT
+ * Text Domain: bp-security-check
+ * Domain Path: /languages/
  */
 
 /**
  * Adds a maths sum to the BuddyPress registration page that the user
  * must answer correctly before registering
- * @version 1.1.0.1
+ * @version 1.2.0
  * @license http://opensource.org/licenses/MIT MIT
  * @author Shea Bunge (http://bungeshea.com)
  */
-class BuddyPress_Security_Check {
 
-	/**
-	 * The prefix prepended to for form fields
-	 * @var string
-	 */
-	private $prefix = '';
+/**
+ * Load up the localization file if we're using WordPress in a different language.
+ */
+function bp_security_check_load_textdomain() {
+	load_plugin_textdomain( 'bp-security-check', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+}
 
-	/**
-	 * Initialize variables and register hooks
-	 * @param string $prefix
-	 */
-	public function __construct( $prefix ) {
-		add_action( 'bp_signup_validate', array( $this, 'check_validation' ) );
-		add_action( 'bp_after_signup_profile_fields', array( $this, 'show_input_field' ) );
+add_action( 'plugins_loaded', 'bp_security_check_load_textdomain' );
 
-		$this->prefix = $prefix;
+/**
+ * Check if the user's input was correct
+ */
+function bp_security_check_validate() {
+	global $bp;
+
+	$sum = get_transient( 'bp-security-check' );
+
+	$a  = $sum[0];
+	$op = $sum[1];
+	$b  = $sum[2];
+
+	$answer = intval( $_POST['bp-security-check'] );
+
+	/* Calculate the actual answer */
+	if ( 2 == $op ) {
+		$result = $a - $b;
+	} else {
+		$result = $a + $b;
 	}
 
-
-	/**
-	 * Retrieve the value for a form field
-	 * @param  string $field_name The name of the field to retrieve the value for
-	 * @return string             The field's value
-	 */
-	private function get_field_value( $field_name ) {
-		$field_value = $_POST[ $this->prefix . $field_name ];
-		return apply_filters( 'bp_security_check_get_field_value', $field_value, $field_name );
+	/* The submitted answer was incorrect */
+	if ( $result !== $answer ) {
+		$bp->signup->errors['security_check'] = __( 'Sorry, please answer the question again', 'bp-security-check' );
 	}
 
-	/**
-	 * Retrieve the answer to a maths problem
-	 * @param  integer $a  The first number
-	 * @param  integer $b  The second number
-	 * @param  integer $op The operator code
-	 * @return integer     The problem's answer
-	 */
-	private function do_sum( $a, $b, $op ) {
-		switch ( $op ) {
-			default:
-			case 1: // addition
-				return $a + $b;
-			case 2: // subtraction
-				return $a - $b;
-			case 3: // multiplication
-				return $a * $b;
-			case 4: //division
-				return $a / $b;
-		}
+	/* The answer field wasn't filled in */
+	elseif ( empty( $answer ) ) {
+		$bp->signup->errors['security_check'] = __( 'This is a required field', 'bp-security-check' );
 	}
 
-	/**
-	 * Retrieve the HTML entity for an operation
-	 * @param  integer $op An operator code
-	 * @return string      An HTML entity
-	 */
-	private function format_operation( $op ) {
-
-		$ops = array(
-			1 => '&#43;',   // addition
-			2 => '&#8722;', // subtraction
-			3 => '&#215;',  // multiplication
-			4 => '&#247;',  // division
-		);
-
-		return ( isset( $ops[ $op ] ) ? $ops[ $op ] : $ops[1] );
+	/* Clean up the transient if the answer was correct */
+	else {
+		delete_transient( 'bp-security-check' );
 	}
+}
 
-	/**
-	 * Check if the user's input was correct
-	 */
-	public function check_validation(){
-		global $bp;
+add_action( 'bp_signup_validate', 'bp_security_check_validate' );
 
-		$number_a  = intval( $this->get_field_value( 'number_a' ) );
-		$number_b  = intval( $this->get_field_value( 'number_b' ) );
-		$answer    = intval( $this->get_field_value( 'answer' ) );
-		$operation = $this->get_field_value( 'operation' );
 
-		if ( $this->do_sum( $number_a, $number_b, $operation ) !== $answer ) {
-			/* The submitted answer was incorrect */
-			$bp->signup->errors['security_check'] = __( 'Sorry, please answer the question again', 'bp-security-check' );
-		}
-		elseif ( empty( $answer ) ) {
-			/* The answer field wasn't filled in */
-			$bp->signup->errors['security_check'] = __( 'This is a required field', 'bp-security-check' );
-		}
-	}
+/**
+ * Render the input fields
+ */
+function bp_security_check_field() {
 
-	/**
-	 * Render the input fields
-	 */
-	public function show_input_field() {
+	/* Get a random number between 0 and 10 (inclusive) */
+	$a = mt_rand( 0, 10 );
+	$b = mt_rand( 0, 10 );
 
-		/* Get a random number between 0 and 10 (inclusive) */
-		$a = mt_rand( 0, 10 );
-		$b = mt_rand( 0, 10 );
+	/* Get a random operation */
+	$op = mt_rand( 1, 2 );
+
+	/* Make adjustments to the numbers for subtraction */
+	if ( 2 == $op ) {
 
 		/* Make sure that $a is greater then $b; if not, switch them */
 		if ( $b > $a ) {
@@ -122,34 +91,45 @@ class BuddyPress_Security_Check {
 			$a = $b;      // assign $a (lower number) to $b (higher number)
 			$b = $_a;     // assign $b to the original $a
 			unset( $_a ); // destroy the backup variable
-		} elseif ( $a == $b ) {
-			$a++;  // Increment $a so that we never get 0 and hit validation errors being required
 		}
-		/* Get a random operation */
-		$op = mt_rand( 1, 2 );
 
-		?>
-		<div style="float: left; clear: left; width: 48%; margin: 12px 0;" class="security-question-section">
-			<h4><?php esc_html_e( 'Security Question', 'bp-security-check' ); ?></h4>
-			<?php do_action( 'bp_security_check_errors' ); ?>
-			<label for="<?php echo $this->prefix; ?>answer" style="display: inline;">
-				<?php printf( '%1$d %3$s %2$d &#61;', $a, $b, $this->format_operation( $op ) ); ?>
-			</label>
-			<input type="hidden" name="<?php echo $this->prefix; ?>number_a" value="<?php echo $a; ?>" />
-			<input type="hidden" name="<?php echo $this->prefix; ?>number_b" value="<?php echo $b; ?>" />
-			<input type="hidden" name="<?php echo $this->prefix; ?>operation" value="<?php echo $op; ?>" />
-			<input type="number" name="<?php echo $this->prefix; ?>answer" min="0" max="20" required="required" />
-		</div>
-		<?php
+		/* If the numbers are equal then the result will be zero, which will cause an error */
+		elseif ( $a == $b ) {
+			$a++;
+		}
 	}
+
+	/* Save sum information (expiry = 12 hours) */
+	set_transient( 'bp-security-check', array( $a, $op, $b ), 12 * 60 * 60 );
+
+	?>
+	<div style="float: left; clear: left; width: 48%; margin: 12px 0;" class="security-question-section">
+		<h4><?php esc_html_e( 'Security Question', 'bp-security-check' ); ?></h4>
+		<?php do_action( 'bp_security_check_errors' ); ?>
+		<label for="bp-security-check" style="display: inline;">
+			<?php
+
+			/* First number */
+			echo $a;
+
+			/* Print operation as proper HTML entity */
+			if ( 2 == $op ) {
+				echo ' &#8722; '; // subtraction
+			} else {
+				echo ' &#43; '; // addition
+			}
+
+			/* Second number */
+			echo $b;
+
+			/* Equals symbol */
+			echo ' &#61;';
+
+			?>
+		</label>
+		<input type="number" name="bp-security-check" required="required" />
+	</div>
+	<?php
 }
 
-/**
- * Initialize the plugin class
- */
-function bp_security_check_init() {
-	$prefix = apply_filters( 'bp_security_check_prefix', 'security_question_' );
-	$GLOBALS['bp_security_check'] = new BuddyPress_Security_Check( $prefix );
-}
-
-add_action( 'bp_init', 'bp_security_check_init' );
+add_action( 'bp_after_signup_profile_fields', 'bp_security_check_field' );
--- readme.txt
+++ readme.txt
@@ -1,8 +1,8 @@
 === BuddyPress Security Check ===
 Contributors: bungeshea
 Tags: math, registration, buddypress, security, anti-spam
-Tested up to: 3.8.1
-Stable tag: 1.1.0.1
+Tested up to: 3.9.1
+Stable tag: 1.2.0
 License: MIT
 License URI: http://opensource.org/licences/MIT
 Donate link: http://bungeshea.com/donate/
@@ -23,12 +23,13 @@ This plugin extends the functionality of [BuddyPress](http://wordpress.org/plugi
 2. Activate the plugin through the __(Plugins > Installed Plugins)__ menu in WordPress
 3. Done! The plugin has no settings to configure, just install and activate
 
-== Frequently Asked Questions ==
-
-= I have a question… =
-Great! I'll be happy to answer your support requests, feedback, and suggestions in the [Support Forums](http://wordpress.org/support/plugin/bp-security-check)
-
 == Changelog ==
+
+= 1.2.0 =
+* Added Hungarian translation by Laszlo Espadas
+* Added Brazilian Portuguese translation by Renato Alves
+* Saved sum information in database instead of hidden fields in an attempt to prevent bots
+* Code refactoring
 
 = 1.1.0.1 =
 * Fixed bug preventing the plugin from loading
@@ -47,6 +48,9 @@ Great! I'll be happy to answer your support requests, feedback, and suggestions 
 * Stable version release
 
 == Upgrade Notice ==
+
+= 1.2.0 =
+New translations plus fixes to prevent bots
 
 = 1.1.0.1 =
 Fixed bug preventing the plugin from loading
